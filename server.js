552283e42acfd0a938870ad3d1c576299882ded3
Anoop555777 <ajbisht99@gmail.com>
2022-10-25 23:03:25 +0530

    set up database

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('DataBase in connected successfully');
+  });
 
 const port = process.env.PORT;
 //server
