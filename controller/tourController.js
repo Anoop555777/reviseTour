744ff654468f627e99e47390e924eb500e8db6db
Anoop555777 <ajbisht99@gmail.com>
2022-10-29 00:17:35 +0530

    sort method

--- controller/tourController.js
+++ controller/tourController.js
@@ -36,7 +36,15 @@ exports.getAllTours = async (req, res) => {
       (match) => `$${match}`
     );
 
-    const query = Tour.find(JSON.parse(queryString));
+    let query = Tour.find(JSON.parse(queryString));
+
+    if (req.query['sort']) {
+      const sortBy = req.query['sort'].split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
     const tours = await query;
     res.status(200).json({
       status: 'success',
